5d73f7e3dff243e46b401311b784e3ef570f14a8
Anne Boon <anneboon@uitgeverij-deviant.nl>
2019-10-11 08:48:56 +0200

    stuff

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace repro
+{
+    public class Test
+    {
+        private string _someValue = "SomeValue";
+        private string _someOtherValue = "SomeOtherValue";
+
+        // CA1822 triggers on this function
+        public string ExpressionBody1(int a) =>
+            a switch
+            {
+                _ => _someValue
+            };
+
+        // CA1822 doesn't trigger on this function
+        public string ExpressionBody2(int a) =>
+            a switch
+            {
+                _ => _someOtherValue
+            };
+    }
+}
